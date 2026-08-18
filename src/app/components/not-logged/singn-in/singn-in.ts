efbffd08e8585b28345e718efec09c088e370140
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2026-08-17 21:23:23 -0300

    feat: configura componentes logados

---
+++ src/app/components/not-logged/singn-in/singn-in.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-singn-in',
+  imports: [],
+  templateUrl: './singn-in.html',
+  styleUrl: './singn-in.css',
+})
+export class SingnIn {}
