ae72ac14ee177e8ab2d6e308181ce6eccf3923fb
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2026-08-17 21:22:09 -0300

    feat: configura páginas logadas

---
+++ src/app/pages/logged/home/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Card } from '../../../components/logged/card/card';
+import { Header } from '../../../components/logged/header/header';
+
+@Component({
+  selector: 'app-home',
+  imports: [Header, Card],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {}
