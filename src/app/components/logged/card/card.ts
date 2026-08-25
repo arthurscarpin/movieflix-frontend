7a6c0c87f50307d665abaffcec5743bf459a3902
Arthur Scarpin <scarpinarthur.dev@gmail.com>
2026-08-24 22:10:04 -0300

    style: implementar componente de cartão de filme com layout e estilos detalhados

--- src/app/components/logged/card/card.ts
+++ src/app/components/logged/card/card.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './card.html',
   styleUrl: './card.css',
 })
-export class Card {}
+export class Card {
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() duration: string = '';
+  @Input() ageRating: string = '';
+  @Input() approvalRating: number = 0;
+  @Input() providerLogo: string = '';
+  @Input() isTop10: boolean = false;
+  @Input() releaseDate: string = '';
+
+  getRatingClass(): string {
+    return this.approvalRating >= 50 ? 'rating-good' : 'rating-bad';
+  }
+}
